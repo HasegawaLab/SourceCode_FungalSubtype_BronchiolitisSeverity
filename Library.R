fb0f40014e62e4e470aae509e67d67375ade5ed5
shibataryohei <shibataryohei@gmail.com>
2023-06-30 16:09:45 -0400

    ???

---
+++ Library.R
@@ -0,0 +1,47 @@
+# tidyverse
+library("tidyverse")
+library("dplyr")
+library("readxl")
+library("rstatix")
+library(furrr)
+
+# basic
+library(glue)
+library(knitr)
+
+filter <- dplyr::filter
+select <- dplyr::select
+rename <- dplyr::rename
+summarise <- dplyr::summarise
+
+# bioinformatics
+library(DESeq2)
+library(msigdbr)
+library(goseq)
+library(EnsDb.Hsapiens.v86)
+library(clusterProfiler)
+library(DOSE)
+library(org.Hs.eg.db)
+library(GO.db)
+library(ConsensusClusterPlus)
+
+# epidemiology
+library("survival")
+library("gmodels")
+library("EValue")
+
+# visiualization
+library(ggrepel)
+library(RColorBrewer)
+library(circlize)
+library(ggalluvial)
+library(ggfortify)
+library(GenomeInfoDb)
+
+# Theme
+theme_set(theme_classic())
+theme_update(panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank(),
+             strip.background = element_blank(),
+             # legend.key.size = unit(3, "mm"),
+             plot.margin=unit(c(1, 1, 1, 1),"mm"))
